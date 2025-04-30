dfe52037f865073b7776ae7f19226b60441aa3c9
Ketan Nakum <ketan.nakum77@gmail.com>
2025-04-30 16:51:43 +0530

    change result failure then backoffice policy define like liner

--- app/src/main/java/com/gurukrupa/workmanagerdemo/worker/DemoWorker.kt
+++ app/src/main/java/com/gurukrupa/workmanagerdemo/worker/DemoWorker.kt
@@ -8,7 +8,7 @@ import androidx.work.WorkerParameters
 class DemoWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
     override fun doWork(): Result {
         performWork()
-        return Result.success()
+        return Result.failure()
     }
 
     private fun performWork() {
